3a4fe44cc308d4b897c4c67388b3130cdf9bd9eb
Javier Saldana <javier@javiersaldana.com>
2013-11-25 17:41:15 -0600

    add test case for variable declarations

--- spec/elda_spec.rb
+++ spec/elda_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 describe Rubik::VM do
 
+  it 'declares variables' do
+    [1, 5.0, "hello", false].each do |expected_result|
+      $stdout.should_receive(:print).with(expected_result)
+    end
+
+    program = <<-EOF
+begin
+  int i; float f; string s; boolean b;
+  i = 1; f = 5.0; s = "hello"; b = false;
+
+  print(i . f . s . b);
+end
+EOF
+    Rubik::VM.new program
+  end
+
   pending 'calculates the fibonacci sequence'
 
   it 'calculates the factorial of a given number' do
@@ -9,8 +25,7 @@ describe Rubik::VM do
     program = <<-EOF
 def int factorial(int a) {
   int sum, cont;
-  sum = 1;
-  cont = a;
+  sum = 1; cont = a;
 
   do a times {
     sum = sum * cont;
