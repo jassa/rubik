25dfafb024341d3f9a6cde5292511ade8998f2b1
Javier Saldana <javier@javiersaldana.com>
2013-11-25 18:22:38 -0600

    fix spacing in test cases

--- spec/elda_spec.rb
+++ spec/elda_spec.rb
@@ -2,22 +2,6 @@ require 'spec_helper'
 
 describe Rubik::VM do
 
-  it 'declares variables' do
-    [1, 5.0, "hello", false].each do |expected_result|
-      $stdout.should_receive(:print).with(expected_result)
-    end
-
-    program = <<-EOF
-begin
-  int i; float f; string s; boolean b;
-  i = 1; f = 5.0; s = "hello"; b = false;
-
-  print(i . f . s . b);
-end
-EOF
-    Rubik::VM.new program
-  end
-
   pending 'calculates the fibonacci sequence'
 
   it 'calculates the factorial of a given number' do
